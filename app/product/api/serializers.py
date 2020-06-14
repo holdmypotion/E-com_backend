c78e741d229020ba35e5e4e39a34da87585fdf5b
Elijah <rahulsharma240301@gmail.com>
2020-06-15 05:27:54 +0530

    Add tests for product and section api

--- app/product/api/serializers.py
+++ app/product/api/serializers.py
@@ -3,16 +3,6 @@ from rest_framework import serializers
 from product.models import Product, Section
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    """Serializer for product object"""
-    section = serializers.StringRelatedField()
-
-    class Meta:
-        model = Product
-        fields = ('title', 'description', 'slug', 'active', 'section')
-        read_only_fields = ('slug',)
-
-
 class SectionSerializer(serializers.ModelSerializer):
     """Serializer for Section object"""
     products = serializers.StringRelatedField(many=True)
@@ -20,3 +10,13 @@ class SectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Section
         fields = ('title', 'active', 'products')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """Serializer for product object"""
+    section = SectionSerializer()
+
+    class Meta:
+        model = Product
+        fields = ('title', 'description', 'slug', 'active', 'section')
+        read_only_fields = ('slug',)
--- app/product/tests/test_api.py
+++ app/product/tests/test_api.py
@@ -5,11 +5,16 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from product.models import Product, Section
-from product.api.serializers import ProductSerializer
+from product.api.serializers import ProductSerializer, SectionSerializer
 
 
 PRODUCT_URL = reverse('product:product-list')
 SECTION_URL = reverse('product:section-list')
+
+
+def detail_url(product_id):
+    """Returns product detail URL"""
+    return reverse('product:product-detail', args=[product_id])
 
 
 def sample_section(**params):
@@ -47,5 +52,34 @@ class PublicProductApiTest(TestCase):
 
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_product_detail(self):
+        """Test viewing a product detail"""
+        product = sample_product(description='This is a tape, ofcourse')
+
+        url = detail_url(product.id)
+        res = self.client.get(url)
+
+        serializer = ProductSerializer(product)
+        self.assertEqual(res.data, serializer.data)
+
+
+class PublicSectiontApiTest(TestCase):
+    """Test the publically available Section API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_retrieve_section(self):
+        """Test retrieving products"""
+        sample_section()
+        sample_section(title='Sample 2')
+
+        res = self.client.get(SECTION_URL)
+
+        sections = Section.objects.all()
+        serializer = SectionSerializer(sections, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
