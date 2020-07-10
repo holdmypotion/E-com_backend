7ec636469433ead5daa9c1bedd7f2cbabe22c180
Elijah <rahulsharma240301@gmail.com>
2020-07-10 21:26:45 +0530

    Add Order model

--- app/absolutebond/settings.py
+++ app/absolutebond/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'product',
     'user',
     'address',
+    'order',
 ]
 
 MIDDLEWARE = [
---
+++ app/order/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+# from django.conf import settings
+
+from product.models import Product
+from address.models import Address
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+    # user = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL,
+    #     on_delete=models.CASCADE,
+    #     related_name='orders',
+    #     null=True
+    # )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT,
+    )
+    shipping_address = models.ForeignKey(
+        Address,
+        on_delete=models.PROTECT
+    )
+    quantity = models.CharField(max_length=500)
+    shipping_total = models.DecimalField(
+        default=0.00,
+        max_digits=100,
+        decimal_places=2
+    )
+    total = models.DecimalField(
+        default=0.00,
+        max_digits=100,
+        decimal_places=2
+    )
+    active = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.order_id
+
+    class Meta:
+        ordering = ['-timestamp', '-updated']
+
+    
