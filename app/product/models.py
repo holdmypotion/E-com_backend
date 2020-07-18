194caaef36764d8ade6237dab892178b5eba19a4
Elijah <rahulsharma240301@gmail.com>
2020-07-18 18:58:21 +0530

    Add Fuction to prepopulate slug field

--- app/product/models.py
+++ app/product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Section(models.Model):
@@ -30,3 +31,17 @@ class Product(models.Model):
     def __str__(self):
         """String representation"""
         return self.title
+    
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Product.objects.filter(slug=unique_slug).exists():
+            unique_slug = f'{slug}-{num}'
+            num += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, **kwargs)
